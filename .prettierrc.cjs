0db39292e328dd38cf454a993fecff5db65857f6
Anderson-Vilela <andersonvilela.dev@gmail.com>
2023-02-20 16:40:26 -0300

    starting project settings

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    printWidth: 80,
+    proseWrap: 'always',
+    quoteProps: 'as-needed',
+    requirePragma: false,
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'all',
+    useTabs: false,
+}
